62cae971aa61d41a97f4d4df8e271647e80afa4a
Heberty <hebertyprado22005@gmail.com>
2022-11-29 09:20:33 -0300

    end-point service was configured

--- src/app/endpoint-dog.service.ts
+++ src/app/endpoint-dog.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Dog } from './dog'
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EndpointDogService {
 
+  private dogUrl = 'https://dog.ceo/api/breeds/image/random';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getDog(): Observable<Dog> {
+    return this.http.get<Dog>(this.dogUrl);
+  }
 }
